bc0767b35bc2b198a8a95e2d71956bce7111a9b2
Steven Thomason <thomason84@hotmail.com>
2019-06-21 17:33:06 -0400

    added title description page and minor CSS formating

--- src/components/CurrentlyPlaying.vue
+++ src/components/CurrentlyPlaying.vue
@@ -1,22 +1,33 @@
 <template>
     <div>
-        <div class='movieCard' v-for='movie in movies' v-bind:key='movie.id'>
-        
-            <div>
-                
+        <section v-if='show'>
+            <div class='movieCard' v-for='movie in movies' v-bind:key='movie.id' @click='getMovieDetails(movie)'>
+                <div>
+                    <h1>{{ title }}</h1>
+                </div>
+                <div>
+                    <img :src='imageUrlPartial + movie.backdrop_path' :alt='"backdrop movie image for " + movie.title' :title='"Get more details about " + movie.title'>
+                    <h2>{{ movie.title }}</h2>
+                    <p><span class='bold'>Release Date:</span> {{ movie.release_date }}</p>
+                    <p><span class='bold'>Voter average:</span> {{ movie.vote_average }}</p>
+                    <p><span class='bold'>Total votes:</span> {{ movie.vote_count }}</p>
+                    <p><span class='bold'>Genres:</span><span class='genre' v-for='(genre, index) in movie.genre_ids' v-bind:key='index'>{{ getGenre(genre) }}</span></p>
+                </div>
             </div>
-
-            <div>
-                <img 
-                :src='imageUrlPartial + movie.backdrop_path'
-                :alt='"backdrop movie image for " + movie.title'
-                :title='"Get more details about " + movie.title'>
-
-                <h2>{{ movie.title }}</h2>
-                <p class='movieGenre' v-for='genre in movie.genre_ids'>{{ getGenre(genre) }}</p>
+        </section>
+        <section v-if='!show'>
+            <div class='movieDetails' style="width: 100%; height: auto">
+                <h2>{{ movieTitle }}</h2>
+                <img :src='imageUrlPartial + poster' :alt='"backdrop movie image for " + movieTitle' :title='"Get more details about " + movieTitle'>
+                <p><span class='bold'>Released:</span> {{ releaseDate }}</p>
+                <p><span class='bold'>Score:</span> {{ voteAverage }}</p>
+                <p><span class='bold'>Synopsys:</span> {{ overview }}</p>
+                <p><span class='bold'>Genres:</span> <span class='genre' v-for='genre in genre' v-bind:key='genre'>{{ getGenre(genre) }}</span></p>
+                <div v-for='(video, index) in videos' :key='index' class='videoContainer'>
+                    <iframe class='video' :src='youtubePartial + video.key' frameborder="0" allowfullscreen></iframe>
+                </div>
             </div>
-        </div>
-        
+        </section>
     </div>
 </template>
 
@@ -28,34 +39,45 @@ export default {
     data () {
         return {
         title: 'Movies Currently in Theaters',
-        show: false,
+        show: true,
         loading: false,
         apiKey: 'a611315ba9b178c672528a504436308c',
         urlPartial: 'https://api.themoviedb.org/3/movie/now_playing?api_key=',
         imageUrlPartial: 'https://image.tmdb.org/t/p/w500/',
-        backdrop: '',
+        youtubePartial: 'https://www.youtube.com/embed/',
+        videoUrlPartial1: 'https://api.themoviedb.org/3/movie/',
+        videoUrlPartial2: '?api_key=',
+        videoUrl: '',
+        youTubeVideo: '',
+        movieId: '',
         poster: '',
         releaseDate: '',
         movieTitle: '',
         overview: '',
         genre: '',
-        movies: []
+        voteAverage: '',
+        movies: [],
+        videos: []
         }
     },
     methods: {
         getMovies () {
             const urlString = this.urlPartial + this.apiKey
-        axios
+            axios
             .get(urlString)
             .then(response => {
-                console.log(response)
                 this.movies = response.data.results
+            })
+            .then(data => {
+                let genres = document.querySelectorAll('.genre')
+                let genresArray = Array.from(genres)
+                genresArray[0].classList.add('firstGenre')
             })
             .catch(error => {
                 console.log(error)
             })
         },
-        getGenre (genre) {
+        getGenre (genre, index) {
             if (genre === 28) return 'Action'
             if (genre === 12) return 'Adventure'
             if (genre === 16) return 'Animation'
@@ -75,6 +97,33 @@ export default {
             if (genre === 53) return 'Thriller'
             if (genre === 10752) return 'War'
             if (genre === 37) return 'Western'
+        },
+        getMovieDetails (movieDetails) {
+            console.log(movieDetails)
+            this.show = !this.show
+            this.poster = movieDetails.poster_path
+            this.movieTitle = movieDetails.title
+            this.genre = movieDetails.genre_ids
+            this.releaseDate = movieDetails.release_date
+            this.movieId = movieDetails.id
+            this.videoUrl = this.videoUrlPartial1 + this.movieId + this.videoUrlPartial2 + this.apiKey + '&append_to_response=videos'
+            this.voteAverage = movieDetails.vote_average
+            this.overview = movieDetails.overview
+
+            axios
+            .get(this.videoUrl)
+            .then(response => {
+                console.log(response)
+                this.videos = response.data.videos.results
+                console.log('this is the video list ' + this.videos)
+
+                let genres = document.querySelectorAll('.genre')
+                let genresArray = Array.from(genres)
+                genresArray[0].classList.add('firstGenre')
+            })
+            .catch(error => {
+                console.log(error)
+            })
         }
     },
     beforeMount () {
